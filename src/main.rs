5fc3b8c0e72ba1bb8f2d1653ce66067fee2b4627
carlca <carl.caulkett@gmail.com>
2023-12-24 23:44:55 +0000

    Progress via YouTube.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use csv::Reader;
 use console::Term;
 use mongodb::{Client};
 use mongodb::bson::{Bson, Document};
+use futures::stream::StreamExt;
 
 fn main() -> Result<(), Box<dyn Error>> {
   loop {
     let msg = "H to read csv headers,\n".to_owned() +
-              "P to parse csv data,\n" +
-              "R to read sled data,\n" +
-              "M to setup MongoDB,\n" +
+              "G to get MongoDB fields,\n" +
+              "R to read first 23 MongoDB docs,\n" +
               "or Q to quit...";
     println!("{}", msg);
     let key = read_single_key();
@@ -19,19 +19,14 @@ fn main() -> Result<(), Box<dyn Error>> {
           eprintln!("Error reading CSV headers: {:?}", e);
         }
       }
-      // Some('p') | Some('P') => {
-      //   if let Err(e) = parse_csv_data() {
-      //     eprintln!("Error parsing CSV data: {:?}", e);
-      //   }
-      // }
-      // Some('r') | Some('R') => {
-      //   if let Err(e) = read_sled_data() {
-      //     eprintln!("Error reading sled data: {:?}", e);
-      //   }
-      // }
-      Some('m') | Some('M') => {
-        if let Err(e) = setup_mongodb() {
+      Some('g') | Some('G') => {
+        if let Err(e) = get_mongodb_fields() {
           eprintln!("Error setting up mongodb: {:?}", e);
+        }
+      }
+      Some('r') | Some('R') => {
+        if let Err(e) = read_first_23_docs() {
+          eprintln!("Error reading first 23 MongoDB docs: {:?}", e);
         }
       }
       Some('q') | Some('Q') => break,
@@ -52,62 +47,8 @@ fn parse_csv_headers() -> Result<(), Box<dyn Error>> {
   Ok(())
 }
 
-// #[tokio::main]
-// async fn parse_csv_data() -> Result<()> {
-//   let db = Config::new().path("my_db").open()?;
-//   let mut rdr = Reader::from_path("./data/Company.csv")?;
-//
-//   // Get the headers
-//   let headers = rdr.headers()?.clone();
-//
-//   // Specify the field names for which keys should be created
-//   let key_field_names = ["CompanyName", "RegAddress.PostTown", "RegAddress.PostCode"];
-//
-//   // Print the headers
-//   println!("{:?}", headers);
-//
-//   let mut count = 0;
-//   for result in rdr.records() {
-//     if let Ok(record) = result {
-//       let primary_key = &record[0];
-//       for (i, value) in record.iter().enumerate().skip(1) {
-//         let header_name = headers.get(i).unwrap_or(&"");
-//
-//         // Check if the header name is one of the specified keys
-//         if key_field_names.contains(&header_name) {
-//           let key = format!("{}_{}", primary_key, header_name);
-//           db.insert(key.as_bytes(), value.as_bytes())?;
-//         }
-//       }
-//       count += 1;
-//       print!("\rRecords processed: {} ({:.4}% done)", count, (count as f64 / 5467419.0) * 100.0);
-//       std::io::stdout().flush()?; // makes sure the printed text appears immediately
-//     }
-//   }
-//
-//   println!(); // final newline for clean terminal prompt
-//   db.flush()?;
-//
-//   Ok(())
-// }
-
-// #[tokio::main]
-// async fn read_sled_data() -> Result<()> {
-//   let db: Db = Config::new().path("my_db").open()?;
-//
-//   let iter = db.iter();
-//   for item in iter {
-//     let (key, value) = item?;
-//     println!("Key: {}, Value: {}",
-//              std::str::from_utf8(&key).unwrap_or("Invalid UTF-8 sequence"),
-//              std::str::from_utf8(&value).unwrap_or("Invalid UTF-8 sequence"),
-//     );
-//   }
-//   Ok(())
-// }
-
 #[tokio::main]
-async fn setup_mongodb() -> Result<(), mongodb::error::Error> {
+async fn get_mongodb_fields() -> Result<(), mongodb::error::Error> {
   let client = Client::with_uri_str("mongodb://localhost:27017").await?;
   let databases = client.list_database_names(None, None).await?;
 
@@ -130,12 +71,31 @@ async fn setup_mongodb() -> Result<(), mongodb::error::Error> {
   Ok(())
 }
 
+#[tokio::main]
+async fn read_first_23_docs() -> Result<(), Box<dyn Error>> {
+  let client = Client::with_uri_str("mongodb://localhost:27017").await?;
+  let companies = client.database("local").collection("company");
+  let mut cursor = companies.find(None, None).await?;
+  let mut count = 0;
+  while let Some(result) = cursor.next().await {
+    let comp: Document = result?;
+    println!("Company: {}", comp.get_str("CompanyName")?);
+    count += 1;
+    if count == 23 {
+      break;
+    }
+  }
+
+  Ok(())
+}
+
 fn print_fields(document: &Document, indent: String) {
   for (key, value) in document.iter() {
     match value {
       Bson::Document(nested_doc) => {
         println!("{}Field name: '{}', Type: 'Document'", indent, key);
-        print_fields(nested_doc, format!("{}  ", indent));  // Recurse into the nested document with increased indentation
+        // Recurse into the nested document with increased indentation
+        print_fields(nested_doc, format!("{}  ", indent));
       },
       _ => {
         let field_type = match_type(&value);
